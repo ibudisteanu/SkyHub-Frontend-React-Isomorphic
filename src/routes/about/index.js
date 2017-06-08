7893080fdb952cf610b2d98ed2ed5cb37ca34b01
Alexandru <ionutbudisteanu@yahoo.com>
2017-06-08 21:16:02 +0300

    Initial Commit of using React Isomorphic

---
+++ src/routes/about/index.js
@@ -0,0 +1,32 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Layout from '../../components/template/Layout';
+import Page from '../../components/Page';
+
+export default {
+
+  path: '/about',
+
+  async action() {
+    const data = await require.ensure([], require => require('./about.md'), 'about');
+
+    return {
+      title: data.title,
+      description: 'About SkyHub',
+      chunk: 'about',
+      component:
+        <Layout>
+          <Page {...data} />
+        </Layout>,
+    };
+  },
+
+};
